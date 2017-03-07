4148c3fc7c653a3e3fca607cb806248c692e7f5f
machos <razgriz9@gmail.com>
2017-03-07 14:46:14 +0000

    modrei rest connected with urls

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from api.views import *
 urlpatterns = [
     url(r'artists_web',ArtistListWeb.as_view()),
     url(r'artists',ArtistList),
+    url(r'modrei_web',ModreiListWeb.as_view()),
+    url(r'modrei',ModreiList),
 ]
