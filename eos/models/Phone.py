37a9189d6b8281caa8bb0f67e73eae3b19305b5b
sairous <valkaniotist@gmail.com>
2017-03-23 17:28:02 +0200

    full clean last commit.

--- eos/models/Phone.py
+++ eos/models/Phone.py
@@ -6,33 +6,31 @@ import json
 class Phone(models.Model):
 
     imsi = models.CharField(max_length = 16, primary_key = True)
-    operator = models.CharField(max_length = 20, blank = True)
+    operator = models.CharField(max_length = 20, blank = True, null = True)
     signal_q = models.PositiveSmallIntegerField(blank = True, null = True)
-    reg_status = models.CharField(max_length = 32, blank = True)
+    reg_status = models.CharField(max_length = 32, blank = True, null = True)
     cipher_ind = models.NullBooleanField(blank = True, null = True)
-    kc = models.CharField(max_length = 20, blank = True)
-    kc_gprs = models.CharField(max_length = 20, blank = True)
-    cipher_key = models.CharField(max_length = 32, blank = True)
-    integrity_key = models.CharField(max_length = 32, blank = True)
-    tmsi = models.CharField(max_length = 20, blank = True)
+    kc = models.CharField(max_length = 20, blank = True, null = True)
+    kc_gprs = models.CharField(max_length = 20, blank = True, null = True)
+    cipher_key = models.CharField(max_length = 32, blank = True, null = True)
+    integrity_key = models.CharField(max_length = 32, blank = True, null = True)
+    tmsi = models.CharField(max_length = 20, blank = True, null = True)
     tmsi_time = models.PositiveIntegerField(blank = True, null = True)
-    lai = models.CharField(max_length = 12, blank = True)
-    ptmsi = models.CharField(max_length = 8, blank = True)
-    ptmsi_sign = models.CharField(max_length = 6, blank = True)
-    rai = models.CharField(max_length = 10, blank = True)
+    lai = models.CharField(max_length = 12, blank = True, null = True)
+    ptmsi = models.CharField(max_length = 8, blank = True, null = True)
+    ptmsi_sign = models.CharField(max_length = 6, blank = True, null = True)
+    rai = models.CharField(max_length = 10, blank = True, null = True)
     threshold = models.PositiveIntegerField(blank = True, null = True)
-    phone_vendor = models.CharField(blank = True, max_length = 30)
-    phone_model = models.CharField(blank = True, max_length = 10)
-    firmware_version = models.CharField(blank = True, max_length =10)
-    imei = models.CharField(blank = True, max_length = 17)
+    phone_vendor = models.CharField(blank = True, max_length = 30, null = True)
+    phone_model = models.CharField(blank = True, max_length = 10, null = True)
+    firmware_version = models.CharField(blank = True, max_length =10, null = True)
+    imei = models.CharField(blank = True, max_length = 17, null = True)
 
     def __str__(self):
         return self.imsi
 
     def get_self(self):
-        #thats another comment test for branch
         return json.dumps({"imsi":self.imsi})
 
     def get_fields(self):
-        #thats a comment from branch testing
         return [(field.name, field.value_to_string(self)) for field in Phone._meta.fields]
