2b34c88cb42f1801d64cb9e6647df569ef117bf3
sairous <valkaniotist@gmail.com>
2017-04-06 13:49:00 +0300

    phone detail page will no longer display logs. Get them via the api. /api/logs/{imsi}?fields={wanted fields separated with ,} /api/logs/123456?fields=kc,operator

--- eos/views/PhoneDetail.py
+++ eos/views/PhoneDetail.py
@@ -6,8 +6,3 @@ class PhoneDetail(DetailView):
     context_object_name = "phone"
     template_name = "phone_detail.html"
     pk_url_kwarg = "imsi"
-
-    def get_context_data(self, **kwargs):
-        context = super(PhoneDetail,self).get_context_data(**kwargs)
-        context['logs'] = Log.objects.filter(imsi = self.object.imsi)
-        return context
