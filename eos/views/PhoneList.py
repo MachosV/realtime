59d69ef4a2e5acafbf84924af967bf7c84f8ece4
sairous <valkaniotist@gmail.com>
2017-04-12 17:48:42 +0300

    minor changes to api. log serializer will no longer return the imsi turned phone create and phone update to json only

--- eos/views/PhoneList.py
+++ eos/views/PhoneList.py
@@ -3,7 +3,7 @@ from eos.models import Phone
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 
-@method_decorator(cache_page(20), name='dispatch')
+#@method_decorator(cache_page(20), name='dispatch')
 class PhoneList(ListView):
     template_name = "phones.html"
     model = Phone
