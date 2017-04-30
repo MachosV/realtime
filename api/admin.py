3c881d9926c618fb5080cde6fc520c995ecba78c
sairous <valkaniotist@gmail.com>
2017-04-30 15:20:42 +0300

    Added model registering on admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
+app_models = apps.get_app_config('eos').get_models()
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
